8df9e1f3071c1a86177a5a4596c7bd80e58807e3
Valkora <vcorostinscaia@mitocgroup.com>
2018-02-12 12:24:59 +0200

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,18 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
   console.log("New user connected");
 
-  socket.emit('newMessage', {
-    from: "val1",
-    text: "HEEEEEEEEEEEY",
-    createAt: 12345
-  });
-
-  socket.on('createEmail', (newEmail) =>{
-    console.log('createEmail', newEmail);
-  });
-
   socket.on('createMessage', (message) => {
     console.log("createMessage", message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    })
+  });
+
+  socket.on('disconnect', () =>{
+    console.log('User was disconnected');
   });
 });
 
